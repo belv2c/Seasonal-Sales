f7198cfa4854813a2f5138b6037cb6e897ccd509
Brooke Lewis <belv2c@gmail.com>
2017-09-15 22:58:51 -0500

    almost done

--- redo.js
+++ redo.js
@@ -1,5 +1,9 @@
 console.log("seasonal sales");
-
+var seasonDropdown = document.getElementById("season-dropdown");
+var seasonSelected = seasonDropdown.value;
+var seasonDiscountPrice = [];
+var finalPrice = document.getElementsByClassName("the-price");
+var arrayArray = [];
 
 //XHR//
 function executeThisCodeAfterFileLoads(){
@@ -40,6 +44,7 @@ function addCategoriesToProducts(productsArray, categoriesArray){
 				product["category"] = category.name;
 				product["season"] = category["season_discount"];
 				product["discount"] = category.discount;
+				product["discountPrice"] = product.price - (product.price * product.discount);
 
 			}
 		});
@@ -52,20 +57,39 @@ function addCategoriesToProducts(productsArray, categoriesArray){
 function domString(yay) {
 	var domString = "";
 	for (var i = 0; i < yay.length; i++){
+		if(seasonDropdown.value = yay[i].categoryId){
+			var totalPrice = yay[i].discountedPrice.toFixed(2);
+		} else {
+			var totalPrice= yay[i].price;
+		}
 		domString += `<div class="productCard">`;
 		domString += 	`<h4>${yay[i].category}</h4>`;
 		domString += 	`<h4>${yay[i].name}</h4>`;
-		domString +=  	`<h5>${yay[i].price}</h5>`;
+		domString +=  	`<h5 class="the-price">${yay[i].price}</h5>`;
 		domString +=  `</div>`;
 	}
 
 	writeToDom(domString);
 }
 
+
+
 function writeToDom(strang) {
 	var productContainer = document.getElementById("product-container");
 	productContainer.innerHTML = strang;
 }
+
+
+	seasonDropdown.addEventListener("change", function(event){
+		console.log("change event", event);
+		if(seasonDropdown.value === "1"){
+			console.log("winter selected", 1);
+		} else if(seasonDropdown.value === "2"){
+			console.log("autumn selected", 2);
+		} else if(seasonDropdown.value === "3"){
+			console.log("spring selected", 3);
+		}
+	});
 
 
 
